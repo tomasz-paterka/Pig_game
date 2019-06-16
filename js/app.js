3a1cfdf97e339698f9c054d745e2d5942588ef31
Tomasz Paterka <tomekpaterka@gmail.com>
2019-06-16 19:07:43 +0200

    add file structure, css and html code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-# Pig_game
-
+/*
 GAME RULES:
 
 - The game has 2 players, playing in rounds
@@ -7,3 +6,5 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
+
+*/
