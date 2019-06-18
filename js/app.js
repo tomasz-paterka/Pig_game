4b302d8427232f15d1c3c7cd67207e661df0ffa0
Tomasz Paterka <tomekpaterka@gmail.com>
2019-06-18 22:12:04 +0200

    changed score goal to 100

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     scores[activePlayer] += roundScore;
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
